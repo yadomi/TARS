b8257519790c6c165ecc20c6117ad4d34c244ea2
Felix Yadomi <dev.yadomi@gmail.com>
2015-07-26 05:51:51 +0200

    Add base API requests

--- lib/tars/api.rb
+++ lib/tars/api.rb
@@ -1,3 +1,22 @@
-module API
+require 'json'
+require 'httparty'
 
+module TARS
+  class API
+    include HTTParty
+    base_uri 'api.telegram.org'
+
+    def self.me
+      request 'getMe'
+    end
+
+    def self.updates
+      request 'getUpdates'
+    end
+
+    def self.request(endpoint)
+      post("/bot#{TARS.config.token}/#{endpoint}")
+    end
+    private_class_method :request
+  end
 end
