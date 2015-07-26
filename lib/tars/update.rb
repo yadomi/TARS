953a70969a9cc79b6636f47c59a2be6476450d72
Felix Yadomi <dev.yadomi@gmail.com>
2015-07-26 22:37:18 +0200

    Dynamicaly set properties

--- lib/tars/update.rb
+++ lib/tars/update.rb
@@ -1,7 +1,14 @@
 module TARS
   class Update
+    class << self
+      attr_reader :message, :update_id
+    end
+
     def initialize(update)
-      p update
+      update.each do |k, v|
+        instance_variable_set("@#{k}", v) unless v.nil?
+      end
+      TARS.bot.execute(@message['text'], self)
     end
   end
 end
