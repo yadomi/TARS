833bc9db2bdd89de19690b1b1f49d3b6f7e44bdb
Felix Yadomi <dev.yadomi@gmail.com>
2015-07-26 22:38:33 +0200

    Implement sendMessage

--- lib/tars/api.rb
+++ lib/tars/api.rb
@@ -18,6 +18,10 @@ module TARS
       request 'setWebhook', url: URI(TARS.config.webhook)
     end
 
+    def self.reply_to(chat_id, text)
+      request 'sendMessage', chat_id: chat_id, text: text
+    end
+
     def self.request(endpoint, options = {})
       post("/bot#{TARS.config.token}/#{endpoint}", query: options)
     end
