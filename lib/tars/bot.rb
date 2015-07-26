242e9cbdb8978dc2aa2c3bbafa0e7c4cc78598c5
Felix Yadomi <dev.yadomi@gmail.com>
2015-07-26 19:18:39 +0200

    Add dummy webrick server

--- lib/tars/bot.rb
+++ lib/tars/bot.rb
@@ -1,3 +1,20 @@
-module Bot
+require 'webrick'
 
+module TARS
+  class PostHandler < WEBrick::HTTPServlet::AbstractServlet
+    def do_POST request, response
+      response.status = 200
+      response['Content-Type'] = 'text/plain'
+      response.body = 'Hello, World!'
+    end
+  end
+
+  class Bot
+    def initialize
+      @server = WEBrick::HTTPServer.new Port: TARS.config.server[:port]
+      @server.mount TARS.config.server[:path], TARS::PostHandler
+      trap 'INT' do @server.shutdown end
+      @server.start
+    end
+  end
 end
