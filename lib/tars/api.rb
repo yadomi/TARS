84171055b1cf8feaf98166db32644c2721e75c8f
Felix Yadomi <dev.yadomi@gmail.com>
2015-07-27 22:55:47 +0200

    Add implementation of sendPhoto

--- lib/tars/api.rb
+++ lib/tars/api.rb
@@ -1,10 +1,15 @@
 require 'json'
 require 'httparty'
+require 'rest-client'
+require 'pp'
+require 'open-uri'
+
+BASE_URI = 'api.telegram.org'
 
 module TARS
   class API
     include HTTParty
-    base_uri 'api.telegram.org'
+    base_uri BASE_URI
 
     def self.me
       request 'getMe'
@@ -20,6 +25,15 @@ module TARS
 
     def self.reply_to(chat_id, text)
       request 'sendMessage', chat_id: chat_id, text: text
+    end
+
+    def self.reply_with_photo(chat_id, photo)
+      uri = "https://#{BASE_URI}/bot#{TARS.config.token}/sendPhoto"
+      random = "/tmp/#{Digest::MD5.hexdigest(photo)}#{File.extname(photo)}"
+
+      IO.copy_stream(open(photo), random)
+      RestClient.post(uri, chat_id: chat_id, photo: open(random))
+      File.delete(random)
     end
 
     def self.request(endpoint, options = {})
